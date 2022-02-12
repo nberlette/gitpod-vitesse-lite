a3c77f14d2ff61099d6a2ce69a786d56fdcce8f9
Nicholas Berlette <nick@berlette.com>
2022-02-12 19:50:43 +0000

    updates

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
