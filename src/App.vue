1ae408911b5ccdf706b3d316f2618b47a0030499
Nicholas Berlette <nick@berlette.com>
2022-02-12 16:14:51 -0800

    Update components and pages

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@
 
 <style global>
   html, body, #app {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
 
-html.dark {
-  background: #121212;
-}
+  html.dark {
+    background: #121212;
+  }
 </style>
