0147798b33143c6c37aaab551a5da503b45931ea
Nicholas Berlette <nick@berlette.com>
2022-02-12 16:15:45 -0800

    update vite.config.ts with new icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /// <reference types="vitest" />
-
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
@@ -39,14 +39,23 @@ export default defineConfig({
     // https://github.com/antfu/unocss
     Unocss({
       shortcuts: [
-        ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-        ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+        [
+          'btn',
+          'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+        ],
+        [
+          'icon-btn',
+          'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-500 ease-in-out hover:opacity-100 hover:text-[#ffae33]',
+        ],
       ],
       presets: [
         presetUno(),
         presetAttributify(),
         presetIcons({
           scale: 1.2,
+          collections: {
+            simple: () => require('@iconify-json/simple-icons').icons,
+          },
         }),
       ],
     }),
